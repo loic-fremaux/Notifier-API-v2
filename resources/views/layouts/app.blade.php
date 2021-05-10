427b0b8325fcba4e871ff756eda9e0eda5244ed4
Loïc Frémaux <loic.fremaux1@gmail.com>
2021-05-10 22:51:38 +0200

    Add container to navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,61 +21,63 @@
 </head>
 <body>
 <div id="app" class="container-lg">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-dark bg-dark">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <a class="navbar-brand" href="{{ route('home') }}">{{ config('app.name') }}</a>
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="navbar-nav">
-                @auth
-                    <li class="nav-item">
-                        <a class="nav-link {{ request()->is('profile*') ? 'active' : '' }}"
-                           href="{{ route('profile') }}">{{ __('nav.profile') }}</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link {{ request()->is('services*') ? 'active' : '' }}"
-                           href="{{ route('services') }}">{{ __('nav.services') }}</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link {{ request()->is('api*') ? 'active' : '' }}"
-                           href="{{ route('user.api') }}">{{ __('nav.api') }}</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link {{ request()->is('firebase*') ? 'active' : '' }}"
-                           href="{{ route('firebase') }}">{{ __('nav.firebase') }}</a>
-                    </li>
-                @endauth
-            </ul>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="{{ route('home') }}">{{ config('app.name') }}</a>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="navbar-nav">
+                    @auth
+                        <li class="nav-item">
+                            <a class="nav-link {{ request()->is('profile*') ? 'active' : '' }}"
+                               href="{{ route('profile') }}">{{ __('nav.profile') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link {{ request()->is('services*') ? 'active' : '' }}"
+                               href="{{ route('services') }}">{{ __('nav.services') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link {{ request()->is('api*') ? 'active' : '' }}"
+                               href="{{ route('user.api') }}">{{ __('nav.api') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link {{ request()->is('firebase*') ? 'active' : '' }}"
+                               href="{{ route('firebase') }}">{{ __('nav.firebase') }}</a>
+                        </li>
+                    @endauth
+                </ul>
 
-            <ul class="navbar-nav ml-md-auto">
-                @guest
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('auth.login') }}</a>
-                    </li>
-                    <li class="nav-item active">
-                        <a class="nav-link" href="{{ route('register') }}">{{ __('auth.create') }}</a>
-                    </li>
-                @else
-                    <li class="nav-item dropdown active">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }}
-                        </a>
-
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                {{ __('auth.logout') }}
+                <ul class="navbar-nav ml-md-auto">
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('auth.login') }}</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('auth.create') }}</a>
+                        </li>
+                    @else
+                        <li class="nav-item dropdown active">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
                             </a>
 
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                @endguest
-            </ul>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                    {{ __('auth.logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+                </ul>
+        </div>
         </div>
     </nav>
 
